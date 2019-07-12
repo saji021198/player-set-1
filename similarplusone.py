b5dc1008a3262de3082125cc443236c7b92d150c
saji021198 <52283311+saji021198@users.noreply.github.com>
2019-07-12 11:05:41 -0700

    Create similarplusone.py

---
+++ similarplusone.py
@@ -0,0 +1,12 @@
+n1,k=map(str,input().split())
+n1=list(n1)
+k=list(k)
+count=0
+for i in range(0,len(n1)):
+        if(n[i]!=k[i]):
+            count=count+1
+if(count==1):
+    print("yes")
+else:
+    print("no")
+        
